d9c827617bb2e0db3a9b3cac38f0e02c71aef685
Unknown <jakub.plesnik@gmail.com>
2018-10-07 22:09:06 +0200

    Add empty class

---
+++ VIScv3/Model/Worksheet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIScv3.Model
+{
+    public class Worksheet
+    {
+        int Id;
+
+        int Month;
+
+        int Hours;
+
+        Customer Customer;
+    }
+}

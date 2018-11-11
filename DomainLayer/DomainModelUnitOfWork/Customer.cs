a3936ce14986744de9383e758742cc464bcb2b74
Jakub Plesnik <jakub.plesnik@sde.cz>
2018-11-11 14:56:14 +0100

    Add unit of work

---
+++ DomainLayer/DomainModelUnitOfWork/Customer.cs
@@ -0,0 +1,53 @@
+﻿using DomainLayer.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.DomainModelUnitOfWork
+{
+    public class Customer : IUnitOfWorkCompatible
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Salary { get; set; }
+
+        public int Age { get; set; }
+
+        public string Adrress { get; set; }
+
+        public Customer()
+        {
+            
+        }
+
+        public void IncreaseSallaryByPercentige( int percentige)
+        {
+            Salary = Salary * (1 + ((double)percentige/100));
+        }
+
+        public string Print()
+        {
+            return "Name:" + this.Name + " Age:" + this.Age;
+        }
+
+        public void MarkNew()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarkDirty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarkRemoved()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DomainLayer/DomainModelUnitOfWork/UnitOfWork.cs
@@ -0,0 +1,59 @@
+﻿using DomainLayer.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DomainLayer.DomainModelUnitOfWork
+{
+    public class UnitOfWork
+    {
+        private List<Customer> newObjects = new List<Customer>();
+        private List<Customer> dirtyObjects = new List<Customer>();
+        private List<Customer> removedObjects = new List<Customer>();
+
+        private static ThreadLocal<UnitOfWork> threadLocal = new ThreadLocal<UnitOfWork>();
+
+        public UnitOfWork()
+        {
+            threadLocal.Value = new UnitOfWork();
+        }
+
+        public static UnitOfWork getCurrent()
+        {
+            if (!threadLocal.IsValueCreated)
+                new UnitOfWork();
+            return threadLocal.Value;
+        }
+
+        public void registerNew()
+        {
+
+        }
+
+        public void registerDirty()
+        {
+
+        }
+
+        public void registerRemove()
+        {
+
+        }
+
+        public void Commit()
+        {
+            // insert new
+            // update dirty
+            // delete removed
+        }
+
+        public void Rollback()
+        {
+            // clear all ques
+        }
+
+    }
+}
